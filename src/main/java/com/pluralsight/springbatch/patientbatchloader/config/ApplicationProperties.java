c21676517c310f6c825ededcbe15ab622ba46ebf
Dave Kalu <davekalu46@gmail.com>
2020-01-23 17:47:03 +0100

    Updates

--- src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
+++ src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
@@ -9,6 +9,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private final Batch batch = new Batch();
 
+    public Batch getBatch() {
+        return batch;
+    }
 
+    public static class Batch {
+        private String inputPath = "/Users/dkalu/Desktop/Projects/Spring-batch/demo/patient-batch-loader/data";
+
+        public String getInputPath() {
+            return this.inputPath;
+        }
+
+        public void setInputPath(String inputPath) {
+            this.inputPath = inputPath;
+        }
+    }
 }
